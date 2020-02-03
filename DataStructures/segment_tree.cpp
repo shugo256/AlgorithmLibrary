aca6b5b015e0af9846933826fbd3d0d092abbc71
shugo256 <shugo256@gmail.com>
2020-02-03 12:43:00 +0900

    fix segtree upfunc

--- DataStructures/segment_tree.cpp
+++ DataStructures/segment_tree.cpp
@@ -1,6 +1,9 @@
 /*
 verified on 2019/8/31
 http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=3840559
+
+fix up_func, verified on 2020/2/3
+http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=4152203
 */
 
 #include <iostream>
@@ -9,23 +12,24 @@ http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=3840559
 using namespace std;
 
 
-/* ここからスニペット */
+/* snippet starts */
 
 #include <functional>
 #include <limits>
 
-// i番目をxにかえるだけの関数(点更新のデフォルト) (i++はwarnig回避)
-#define CHANGE_I [](int i, T x) { i++; return x; }
-// i番目にxたす
-#define ADD(sg) [&](int i, T x) { return sg[i] + x; }
+#define ASSIGN_I [](T &ival, T x) { ival = x; }  // i番目にxを代入(点更新のデフォルト)
 
 template <typename T>
 class SegmentTree {
     int n;                       // 葉の数
     T def;                 // 初期値 && 単位元
     vector<T> tree;              // 本体 
-    function<T(T, T)> operation_f; // 区間クエリで使う処理
-    function<T(int, T)> update_f;  // 点更新で使う処理 ただの変更の他、i番目にxをたすみたいなこともできる
+
+    using op_func_t = function<T(T, T)>;
+    using up_func_t = function<void(T&, T)>;
+
+    op_func_t op_func; // 区間クエリで使う処理
+    up_func_t up_func; // 点更新で使う処理 ただの変更の他、i番目にxをたすみたいなこともできる
 
     // 区間[a, b)の総和(と言うか総operation(は？))
     // ノードk=[l, r)を見ている
@@ -34,46 +38,45 @@ class SegmentTree {
         if (a <= l && r <= b) return tree[(size_t)k]; // 完全に包含される場合
         T vl = _query(a, b, k * 2 + 1, l, (l + r) / 2),
           vr = _query(a, b, k * 2 + 2, (l + r) / 2, r);
-        return operation_f(vl, vr);
+        return op_func(vl, vr);
     }
 
   public:
-    SegmentTree(int _n, T _def, function<T(T, T)> _operation_f, function<T(int, T)> _update_f = CHANGE_I)
-        : def(_def), operation_f(_operation_f), update_f(_update_f)  {
+    SegmentTree(int _n, T _def, op_func_t _op_func, up_func_t _up_func = ASSIGN_I)
+        : def(_def), op_func(_op_func), up_func(_up_func)  {
         n = 1;
         while (n < _n) n <<= 1;
         tree = vector<T>((size_t)(2*n - 1), def);
     }
 
     //よく使うやつら min, max, sum
-    SegmentTree(int _n, T _def, string mode, function<T(int, T)> _update_f = CHANGE_I)
+    SegmentTree(int _n, T _def, string mode, up_func_t _up_func = ASSIGN_I)
         : SegmentTree(
             _n, 
             _def,
             mode == "max" ? [](T l, T r) { return max(l, r); } :
            (mode == "min" ? [](T l, T r) { return min(l, r); } :
                             [](T l, T r) { return l + r; }), // sum
-            _update_f
+            _up_func
         ) {}
 
-    SegmentTree(int _n, string mode, function<T(int, T)> _update_f = CHANGE_I)
+    SegmentTree(int _n, string mode, up_func_t _up_func = ASSIGN_I)
         : SegmentTree(
             _n, 
             mode == "max" ? numeric_limits<T>::lowest() : 
            (mode == "min" ? numeric_limits<T>::max() :
                             0), // sum
             mode,
-            _update_f
+            _up_func
         ) {}
 
     template <typename ID>
     void update(ID i, T x) {
-        T next_i = update_f((int)i, x);
         i += (ID)n - 1;
-        tree[(size_t)i] = next_i;
+        up_func(tree[(size_t)i], x);
         while (i > 0) {
             i = (i - 1) / 2;
-            tree[(size_t)i] = operation_f(tree[(size_t)(i * 2 + 1)], tree[(size_t)(i * 2 + 2)]);
+            tree[(size_t)i] = op_func(tree[(size_t)(i * 2 + 1)], tree[(size_t)(i * 2 + 2)]);
         }
     }
 
@@ -104,7 +107,7 @@ SegmentTree(n, def, mode, [up_func])
 SegmentTree(n, mode, [up_func])
 */
 
-/* ここまでスニペット */
+/* snippent ends */
 
 int main() {
     int n, q;
@@ -117,6 +120,5 @@ int main() {
         if (com) cout << sg.query(x, y+1) << '\n';
         else sg.update(x, y);
     }
-    for (auto &si:sg) cout << si << '\n';
     return 0;
 }
